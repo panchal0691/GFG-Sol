ee04367535503b7e67ca8fe00f9e1b67e89fd0f5
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2023-09-09 19:03:54 +0530

    Create Kth largest element in BST.cpp

---
+++ Kth largest element in BST.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int kthLargest(Node *root, int K)
+    {
+        //Your code here
+        int ans;
+        
+        function<void(Node *)> find = [&](Node * cur) {
+            if(cur == nullptr)
+                return;
+                
+            find(cur -> right);
+            
+            --K;
+            if(K == 0)
+                ans = cur -> data;
+                
+            find(cur -> left);
+        };
+        
+        find(root);
+        
+        return ans;
+    }
+};
