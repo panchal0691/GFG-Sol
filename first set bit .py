a26aa98c63465bc0132e0dcbe69a4537daf935a9
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2023-09-19 20:54:40 +0530

    Create first set bit.py

---
+++ first set bit .py
@@ -0,0 +1,14 @@
+class Solution:
+    
+    #Function to find position of first set bit in the given number.
+    def getFirstSetBit(self,n):
+        pos=1
+        
+        while(n != 0):
+            bit=n%2
+            
+            if(bit==1): return pos
+            pos+=1
+            n/=2
+        
+        return 0
