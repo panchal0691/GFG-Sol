b11d3af09f2b8de555e2a13e4b36af978c667b8f
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2023-12-02 23:14:23 +0530

    Create Inorder Traversal and BST.py

---
+++ Inorder Traversal and BST.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def isRepresentingBST(self, arr, N):
+        for i in range(1, N):
+            if arr[i] <= arr[i - 1]:
+                return 0
+        
+        return 1
