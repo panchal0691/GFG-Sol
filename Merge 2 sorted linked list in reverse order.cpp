06f632fe93c6dae6bc594acb3ac363124cec742b
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2024-01-08 20:20:53 +0530

    Create Merge 2 sorted linked list in reverse order.cpp

---
+++ Merge 2 sorted linked list in reverse order.cpp
@@ -0,0 +1,135 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+void print(struct Node *Node)
+{
+    while (Node!=NULL)
+    {
+        cout << Node->data << " ";
+        Node = Node->next;
+    }
+}
+
+
+Node *newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+*/
+class Solution
+{
+    public:
+    
+    Node * reverse(Node * node){
+        Node * prev = nullptr;
+        
+        while(node){
+            Node * next = node -> next;
+            node -> next = prev;
+            prev = node;
+            node = next;
+        }
+        
+        return prev;
+    }
+    
+    struct Node * mergeResult(Node *node1,Node *node2)
+    {
+        node1 = reverse(node1);
+        node2 = reverse(node2);
+        
+        Node * newHead = nullptr;
+        Node * curNode = nullptr;
+        
+        while(node1 and node2){
+            if(node1 -> data > node2 -> data){
+                if(!curNode){
+                    curNode = new Node;
+                    curNode -> data = node1 -> data;
+                    newHead = curNode;
+                }
+                else{
+                    Node * newNode = new Node;
+                    newNode -> data = node1 -> data;
+                    
+                    curNode -> next = newNode;
+                    curNode = newNode;
+                }
+                
+                node1 = node1 -> next;
+            }
+            else{
+                if(!curNode){
+                    curNode = new Node;
+                    curNode -> data = node2 -> data;
+                    newHead = curNode;
+                }
+                else{
+                    Node * newNode = new Node;
+                    newNode -> data = node2 -> data;
+                    curNode -> next = newNode;
+                    curNode = newNode;
+                }   
+                
+                node2 = node2 -> next;
+            }
+        }
+        
+        while(node1){
+            if(!curNode){
+                curNode = new Node;
+                curNode -> data = node1 -> data;
+                newHead = curNode;
+            }
+            else{
+                Node * newNode = new Node;
+                newNode -> data = node1 -> data;
+                curNode -> next = newNode;
+                curNode = newNode;
+            }  
+            
+            node1 = node1 -> next;
+        }
+        
+        while(node2){
+            if(!curNode){
+                curNode = new Node;
+                curNode -> data = node2 -> data;
+                newHead = curNode;
+            }
+            else{
+                Node * newNode = new Node;
+                newNode -> data = node2 -> data;
+                curNode -> next = newNode;
+                curNode = newNode;
+            }  
+            
+            node2 = node2 -> next;
+        }
+        
+        return newHead;
+    }  
+};
